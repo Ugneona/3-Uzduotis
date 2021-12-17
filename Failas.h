06cd111df19de9be092f3149735cac000dac03d1
Ugnė Kniukštaitė <90412167+Ugneona@users.noreply.github.com>
2021-12-17 23:46:58 +0200

    1 bandymas

---
+++ Failas.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Antraste.h"
+#include "Studentas.h"
+
+void failuKurimas(vector <int>& v1, int& ndKiek);
+void darbasSuFailu_nr1(vector <int>& v1, char& atsakymas);
+void darbasSuFailu(vector <int>& v1, char& atsakymas);
+void failas();
